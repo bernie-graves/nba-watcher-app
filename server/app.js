0309a614748bdf9e5cd6b86f27d8cb3288055a4d
bernie-graves <berniejr01@gmail.com>
2022-09-12 02:02:56 -0700

    started twitter stream

--- server/app.js
+++ server/app.js
@@ -7,9 +7,87 @@ const cors = require("cors");
 require("dotenv").config();
 const cookieParser = require("cookie-parser");
 const expressValidator = require("express-validator");
+const needle = require("needle");
 
 // app
 const app = express();
+
+// Twitter Stream
+TOKEN = process.env.TWITTER_BEARER_TOKEN;
+const rulesURL = "https://api.twitter.com/2/tweets/search/stream/rules";
+const streamURL =
+  "https://api.twitter.com/2/tweets/search/stream?tweet.field=public_metrics&expansions=author_id";
+
+const rules = [{ value: "NBA" }];
+
+// Get Twitter Stream rules
+async function getRules() {
+  const response = await needle("get", rulesURL, {
+    headers: {
+      Authorization: `Bearer ${TOKEN}`,
+    },
+  });
+  console.log(response.body);
+  return response.body;
+}
+
+// Set Stream Rules
+async function setRules() {
+  const data = {
+    add: rules,
+  };
+
+  const response = await needle("post", rulesURL, data, {
+    headers: {
+      "content-type": "application/json",
+      Authorization: `Bearer ${TOKEN}`,
+    },
+  });
+  console.log(response.body);
+  return response.body;
+}
+
+// Delete Stream Rules
+async function deleteRules(rules) {
+  if (!Array.isArray(rules.data)) {
+    return null;
+  }
+
+  const ids = rules.data.map((rule) => rule.id);
+
+  const data = {
+    delete: {
+      ids: ids,
+    },
+  };
+
+  const response = await needle("post", rulesURL, data, {
+    headers: {
+      "content-type": "application/json",
+      Authorization: `Bearer ${TOKEN}`,
+    },
+  });
+  console.log(response.body);
+  return response.body;
+}
+
+(async () => {
+  let currentRules;
+
+  try {
+    // get stream rules
+    currentRules = await getRules();
+
+    //delete stream rules
+    await deleteRules(currentRules);
+
+    //set rules based on rules array
+    await setRules();
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+})();
 
 // db
 mongoose
